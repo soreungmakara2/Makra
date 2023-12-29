4b67be849a5e50126ed657e86dcf852991cf71de
Makara <152614418+soreungmakara2@users.noreply.github.com>
2023-12-29 20:29:57 +0700

    Update mkra.py

--- mkra.py
+++ mkra.py
@@ -148,7 +148,7 @@ else:
 			url = url + "/"
 		m = re.search('(https?\://)?([^/]*)/?.*', url)
 		host = m.group(2)
-		for i in range(50000):
+		for i in range(500):
 			t = HTTPThread()
 			t.start()
 		t = MonitorThread()
