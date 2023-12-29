80bd7046b88653b77779b17a0563004cf931b5ee
Makara <152614418+soreungmakara2@users.noreply.github.com>
2023-12-29 22:23:43 +0700

    Update mkra.py

--- mkra.py
+++ mkra.py
@@ -97,7 +97,7 @@ def httpcall(url):
 	except urllib2.HTTPError, e:
 			#print e.code
 			set_flag(1)
-			print '===> CAT (Code)=> 5555'
+			print '===> CAT (Code)=> 5555 ៥៥៥៥'
 			code=5555
 	except urllib2.URLError, e:
 			#print e.reason
