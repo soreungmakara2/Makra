3d11ff93a4fd0d04da7b3c5d0648e5f2dc618d95
Makara <152614418+soreungmakara2@users.noreply.github.com>
2024-03-28 22:21:04 +0700

    Update cat.py

--- cat.py
+++ cat.py
@@ -9,9 +9,10 @@
 from queue import Queue
 from optparse import OptionParser
 import time,sys,socket,threading,logging,urllib.request,random
+
 def user_agent():
-	global uagent  
-        uagent=[]
+	global uagent
+	uagent=[]
 	uagent.append("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14")
 	uagent.append("Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:26.0) Gecko/20100101 Firefox/26.0")
 	uagent.append("Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3")
